5ab134d970648e3edd0b165f459dab32ff766553
quangtien0101 <37357461+quangtien0101@users.noreply.github.com>
2019-12-02 16:23:27 +0700

    Add files via upload

--- client.py
+++ client.py
@@ -2,46 +2,61 @@
 import socket
 import sys
 import subprocess
+import os
+import traceback
+from termios import tcflush, TCIFLUSH
+
 
 def main():
-    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = "127.0.0.1"
     port = 8888
 
     #subprocess.call(['gnome-terminal','-e','python3 client_chat.py'])
 
     try:
-        soc.connect((host, port))
+        connection.connect((host, port))
     except:
         print("Connection error")
         sys.exit()
 
     print("Enter 'quit' to exit")
+    subprocess.call(['gnome-terminal','-e','python3 client_chat.py'])
+    tcflush(sys.stdin, TCIFLUSH)
     message = input("-> ")
+    
 
 
 
     #encrypt message truoc khi gui
 
     while message != 'quit':
-        soc.sendall(message.encode("utf8"))
+        connection.sendall(message.encode("utf8"))
 
-        if "--chat--" in message:
+        if "start-chat" in message:
             print("Opening chat services!")
             subprocess.call(['gnome-terminal','-e','python3 client_chat.py'])
+            tcflush(sys.stdin, TCIFLUSH)
 
-        if "--download" in message:
+        elif "--download" in message:
 
             parsing = message.split()
             items = parsing[1:]
             for i in items:
                 filename = i
-                Download_process(filename, soc)
+                Download_process(filename, connection)
+
+        elif "--upload" in message:
+            parsing = message.split()
+            items = parsing[1:]
+            for i in items:
+                filename = i
+                Upload_process(connection)
 
 
 
         else:
-        	response = soc.recv(5120).decode("utf8")
+        	response = connection.recv(2048).decode("utf8")
         	if response == "-":
         		pass
         	else:
@@ -50,7 +65,7 @@ def main():
 	        
         message = input("-> ")
 
-    soc.send(b'--quit--')
+    connection.send(b'--quit--')
 
 def Download_process(filename, s):
     if filename != 'q':
@@ -75,6 +90,39 @@ def Download_process(filename, s):
         else:
             print ("File Does Not Exist!")
 
+
+def Upload_process(Connection):
+    filename = Connection.recv(2048).decode('utf8')
+    print("receving " +filename)
+    name = filename
+    filename = "/home/wayne/qt/Project_socket/Download/" + filename
+    print("the path is " + filename)
+
+    if os.path.isfile(filename):
+        Connection.send(("EXISTS " + str(os.path.getsize(filename))).encode('utf8'))
+        print("Uploading...")
+        userResponse = Connection.recv(2048).decode('utf8')
+        if userResponse[:2] == 'OK':
+            with open(filename, 'rb') as f:
+                i = 0
+                bytesToSend = f.read(2048)
+                Connection.send(bytesToSend)
+
+                print("sent!" + str(i))
+                i = i + 1
+                while i<100000:
+                    bytesToSend = f.read(2048)
+                    Connection.send(bytesToSend)
+                    print("sent" + str(i))
+                    i = i+1
+
+
+            f.close()
+    else:
+        Connection.send("ERR ".encode('utf8'))
+        print("The file " + name + " doesn't exist!")
+
+    print("Finish Retriving")
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -5,6 +5,8 @@ import traceback
 from threading import Thread
 import subprocess
 import os
+from termios import tcflush, TCIFLUSH
+
 def main():
     start_server()
 
@@ -26,6 +28,8 @@ def start_server():
     soc.listen(5)       # queue up to 5 requests
     print("File server now listening on {}:{} ...".format(host, port))
     subprocess.call(['gnome-terminal','-e','python3 server_chat.py'])
+    tcflush(sys.stdin, TCIFLUSH)
+
 
 
     # infinite loop- do not reset for every requests
@@ -43,10 +47,11 @@ def start_server():
     soc.close()
 
 
-def client_thread(connection, ip, port, max_buffer_size = 5120):
+def client_thread(connection, ip, port, max_buffer_size = 2048):
     is_active = True
 
     while is_active:
+        print("Waiting for incomming transmission...")
         client_input = receive_input(connection, max_buffer_size)
 
         if "--QUIT--" in client_input:
@@ -65,9 +70,8 @@ def client_thread(connection, ip, port, max_buffer_size = 5120):
 
         elif "--upload" in client_input:
             print ("Host request to upload")
+            upload_file(connection, ip, port, client_input)
             
-            #connection.sendall("you request to upload".encode("utf8"))
-            # get the upload file
         
 
         else:
@@ -82,7 +86,7 @@ def receive_input(connection, max_buffer_size):
     if client_input_size > max_buffer_size:
         print("The input size is greater than expected {}".format(client_input_size))
 
-    decoded_input = client_input.decode("utf8").rstrip()  # decode and strip end of line
+    decoded_input = client_input.decode("utf8")
     result = process_input(decoded_input)
 
     return result
@@ -96,36 +100,33 @@ def process_input(input_str):
 def download_file (connection, ip, port, client_input):
     parsing = client_input.split()
 
-    #separte the items need to be download from the command (--download mymom.txt your_dad.csv)
+    #separte the items need to be download from the command (--download Hello.txt goobye.csv)
     items = parsing[2:]
     for i in items:
         print(i)
-        RetrFile(connection)
+        Download_Process(connection)
     
-def RetrFile(sock):
+def Download_Process(sock):
     filename = sock.recv(2048).decode('utf8')
     print(filename)
     filename = "/home/wayne/qt/Project_socket/File_folder/"+filename
 
     if os.path.isfile(filename):
-        sock.send(("EXISTS " + str(os.path.getsize(filename))).encode('utf8'))
+        sock.send(("EXISTS " + str(os.path.getsize(filename))).encode('utf8'))             
         userResponse = sock.recv(2048).decode('utf8')
         if userResponse[:2] == 'OK':
             with open(filename, 'rb') as f:
                 i = 0
-                f.seek(-1,2)
-                eof=f.tell()
-                f.seek(0,0)
                 bytesToSend = f.read(2048)
                 sock.send(bytesToSend)
 
-                #print("sent!" + str(i))
-                i += 1
-                while i<10000:
+                print("sent!" + str(i))
+                i = i + 1
+                while i<100000:
                     bytesToSend = f.read(2048)
                     sock.send(bytesToSend)
-                    #print("sent" + str(i))
-                    i+=1
+                    print("sent" + str(i))
+                    i = i + 1
 
 
             f.close()
@@ -134,6 +135,38 @@ def RetrFile(sock):
 
     print("Finish Retriving")
 
+def upload_file(connection, ip, port, client_input):
+    parsing = client_input.split()
+    items = parsing[2:]
+    for i in items:
+        print(i)
+        Upload_process(i, connection)
+
+
+def Upload_process(filename, connection):
+    if filename != 'q':
+        connection.send(filename.encode('utf8'))
+        print("sending "+filename)
+        data = connection.recv(2048).decode('utf8')
+        print("data: " + data)
+        if data[:6] == 'EXISTS':
+            filesize = int(data[6:])
+            message = 'Y'
+            if message == 'Y':
+                connection.send("OK".encode('utf8'))
+                f = open('/home/wayne/qt/Project_socket/File_folder/'+filename, 'wb')
+                data = connection.recv(2048)
+                totalRecv = len(data)
+                f.write(data)
+                while totalRecv < filesize:
+                    data = connection.recv(2048)
+                    totalRecv += len(data)
+                    f.write(data)
+                    print ("{0:.2f}".format((totalRecv/float(filesize))*100)+ "% Done")
+                print ("upload complete!")
+                f.close()
+        else:
+            print ("File Does Not Exist!")
 
 
 if __name__ == "__main__":
